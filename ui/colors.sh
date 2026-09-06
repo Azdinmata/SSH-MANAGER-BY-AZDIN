4b67ed6b0554b0b4ab865e0a9ef7a5f7080f1627
Azdinmata <159146437+Azdinmata@users.noreply.github.com>
2026-09-06 12:40:30 +0100

    Update colors.sh

--- ui/colors.sh
+++ ui/colors.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
-# ========================================================
-# UI THEME: COLORS & TEXT FORMATTING
-# ========================================================
+export LC_ALL=C
 
 export C_RESET="\033[0m"
 export C_BOLD="\033[1m"
 export C_DIM="\033[2m"
 
-# الألوان الأساسية
 export C_CYAN="\033[38;5;51m"
 export C_BLUE="\033[38;5;39m"
 export C_PURPLE="\033[38;5;141m"
@@ -16,9 +13,3 @@ export C_YELLOW="\033[38;5;220m"
 export C_RED="\033[38;5;196m"
 export C_GRAY="\033[38;5;244m"
 export C_WHITE="\033[38;5;255m"
-
-# خلفيات للأزرار والتنبيهات
-export BG_BLUE="\033[48;5;24m"
-export BG_PURPLE="\033[48;5;54m"
-export BG_GREEN="\033[48;5;22m"
-export BG_RED="\033[48;5;52m"
